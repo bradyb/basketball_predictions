9929beacb28280ba91b943e0f574f5a1db613d7b
bees <brady.ben12@gmail.com>
2019-09-09 08:39:06 -0700

    starting to collect game stats

---
+++ scraper/games.py
@@ -0,0 +1,16 @@
+from sportsreference.nba.boxscore import Boxscore
+import datetime
+import pandas as pd
+
+if __name__ == "__main__":
+    years = ['2014', '2015', '2016', '2017', '2018', '2019']
+    for year in years:
+        file_path = year + '_games.txt'
+        opened_file = open(file_path, 'r')
+        line = opened_file.readline()
+        season_dfs = []
+        while line:
+            boxscore = Boxscore(line.rstrip())
+            season_dfs.append(boxscore.dataframe)
+            line = opened_file.readline()
+        pd.concat(season_dfs).to_pickle(year + '.pkl')
