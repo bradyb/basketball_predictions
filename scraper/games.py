9b1a3ed141def086df8d4fd3af0c61348e2c9f75
bees <brady.ben12@gmail.com>
2019-09-10 20:06:58 -0700

    done fetching game data

--- scraper/games.py
+++ scraper/games.py
@@ -3,7 +3,7 @@ import datetime
 import pandas as pd
 
 if __name__ == "__main__":
-    years = ['2014', '2015', '2016', '2017', '2018', '2019']
+    years = ['2019']#, '2019']
     for year in years:
         file_path = year + '_games.txt'
         opened_file = open(file_path, 'r')
